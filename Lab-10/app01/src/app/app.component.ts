6e93097e862bc9b6ffeca60ef7a8bd2eb8c325fc
MarceloSanabria <108100801+MarceloxASG@users.noreply.github.com>
2024-05-16 01:37:22 -0500

    Add files via upload

---
+++ Lab-10/app01/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Persona } from './persona';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  
+  dia?:string;
+  
+  title = 'app01';
+}
